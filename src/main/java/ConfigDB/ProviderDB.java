5f99f142b66cd742a6328846261ca6f4c6b3f1b2
AlisherNurgazin <71005736+AlisherNurgazin@users.noreply.github.com>
2021-03-15 07:02:05 +0600

    midterm

--- src/main/java/ConfigDB/ProviderDB.java
+++ src/main/java/ConfigDB/ProviderDB.java
@@ -2,6 +2,6 @@ package ConfigDB;
 
 public interface ProviderDB {
     String username = "postgres";
-    String password = "madiloh";
+    String password = "3839";
     String url = "jdbc:postgresql://localhost:5432/ticket";
 }
